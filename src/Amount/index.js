3b738c7eda0f603f945114bbd2c805eb1f9fe622
Przemysław Pacek <ppacek@op.pl>
2023-11-13 15:55:44 +0100

    renamed "content" to "amount"

--- src/Amount/index.js
+++ src/Amount/index.js
@@ -1,27 +1,24 @@
-import { useState } from "react";
 import "./style.css";
 
-const Content = ({ title, amount }) => {
-    const [ content, setContent ] = useState()
+const Amount = ({ title, amount, setAmount }) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        amount(content());
     };
 
     return (
         <form onSubmit={onFormSubmit}>
             <span className="title">{title}</span>
             <input
-                className="content"
+                className="amount"
                 required type="number"
                 step="0.01"
                 min="0.01"
-                value={content}
-                onChange={({ target }) => setContent(target.value)}
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
             />
         </form>
     );
 };
 
-export default Content;
+export default Amount;
