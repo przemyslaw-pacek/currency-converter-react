7df306ffac761c192f3682e23ba6e96def87e3b0
Przemysław Pacek <ppacek@op.pl>
2023-11-14 18:22:02 +0100

    moved currencies in the separate file

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { name: "PLN", number: 1, symbol: "zł" },
+    { name: "EUR", number: 4.46, symbol: "€" },
+    { name: "GBP", number: 5.19, symbol: "£" },
+    { name: "USD", number: 3.97, symbol: "$" },
+];
+
+export default currencies;
