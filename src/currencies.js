e845d444cb75e33ee86bb742954f98266fc6902e
Przemysław Pacek <ppacek@op.pl>
2023-11-17 22:19:00 +0100

    updated exchange rates

--- src/currencies.js
+++ src/currencies.js
@@ -1,8 +1,8 @@
 const currencies = [
     { name: "PLN", number: 1, symbol: "zł" },
-    { name: "EUR", number: 4.46, symbol: "€" },
-    { name: "GBP", number: 5.19, symbol: "£" },
-    { name: "USD", number: 3.97, symbol: "$" },
+    { name: "EUR", number: 4.3767, symbol: "€" },
+    { name: "GBP", number: 5.0048, symbol: "£" },
+    { name: "USD", number: 4.0327, symbol: "$" },
 ];
 
 export default currencies;
