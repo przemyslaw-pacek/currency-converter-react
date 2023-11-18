67263ab01a1fcbe86bbf046a719775dc0dc5211b
Przemysław Pacek <ppacek@op.pl>
2023-11-18 12:04:13 +0100

    added paragraph

--- src/Result/index.js
+++ src/Result/index.js
@@ -4,7 +4,7 @@ const Result = ({ title, result }) => (
     <p>
         <span className="title">{title}</span>
         <p className="result">
-        {result ? `${result.resultAmount.toFixed(2)} ${result.symbolCurrency}` : null}
+            {result ? `${result.resultAmount.toFixed(2)} ${result.symbolCurrency}` : null}
         </p>
     </p>
 )
