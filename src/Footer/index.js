4e8f1df2dc39c1614f770744caabb5eee602e4a4
Przemysław Pacek <ppacek@op.pl>
2023-12-17 14:21:54 +0100

    added Footer component

---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+import { Info } from "./styled.js";
+
+const Footer = () => (
+    <Info> Kursy walut pobierane są z Europejskiego Banku Centralnego.
+        Aktualne na dzień: <strong>xxx</strong>
+    </Info>
+)
+
+export default Footer;
---
+++ src/Footer/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Info = styled.p`
+    font-size: small;
+    text-align: center;
+`;
