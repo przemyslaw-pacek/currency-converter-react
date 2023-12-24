32bb88c59aede5d75c87aeb7ff7b8d50c4e6c052
Przemysław Pacek <ppacek@op.pl>
2023-12-24 13:13:19 +0100

    moved colors to theme

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    color: {
+        white: "#ffffff",
+        yellow: "#ffff00",
+        red: "#ff0000",
+        blackTransparent: "rgba(60, 60, 60, 0.505)",
+        mongoose: "##b6ac7a",
+    },
+}
