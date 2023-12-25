3333a2679838e9f6562e3617be887abb0e617a30
Przemysław Pacek <ppacek@op.pl>
2023-12-25 16:02:45 +0100

    changed format of date

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,13 +3,13 @@ import { Info } from "./styled.js";
 const Footer = ({ data }) => {
     const isoDate = Object.values(data.meta);
     const date = new Date(isoDate[0]);
-    const format = (item) => item.toLocaleString();
+    const formatDate = date.toLocaleString();
 
     return (
         <>
             {data.status === "content" && (
                 <Info> Kursy walut pobierane są z Europejskiego Banku Centralnego.
-                    Aktualne na dzień: <strong>{format(date)}</strong>
+                    Aktualne na dzień: <strong>{formatDate}</strong>
                 </Info>
             )}
         </>
