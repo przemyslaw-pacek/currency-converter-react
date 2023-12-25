cea89279150f88b3318c92e85c394968f082acd7
Przemysław Pacek <ppacek@op.pl>
2023-12-25 14:53:58 +0100

    removed file

--- src/currencies.js
+++
@@ -1,8 +0,0 @@
-const currencies = [
-    { name: "PLN", rate: 1, symbol: "zł" },
-    { name: "EUR", rate: 4.3767, symbol: "€" },
-    { name: "GBP", rate: 5.0048, symbol: "£" },
-    { name: "USD", rate: 4.0327, symbol: "$" },
-];
-
-export default currencies;
