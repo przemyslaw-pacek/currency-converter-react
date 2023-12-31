b920df916f120e065ac77237ac92d5ffe5052742
Przemysław Pacek <ppacek@op.pl>
2023-12-31 18:41:16 +0100

    removed onClick from Amount

--- src/Amount/index.js
+++ src/Amount/index.js
@@ -1,7 +1,7 @@
 import { Title } from "../GlobalStyle.js"
 import { Input } from "./styled.js";
 
-const Amount = ({ data, title, amount, setAmount, exchangeRate }) => (
+const Amount = ({ data, title, amount, setAmount }) => (
 
     <>
         {data.status === "content" && (
@@ -14,7 +14,6 @@ const Amount = ({ data, title, amount, setAmount, exchangeRate }) => (
                         min="0.01"
                         value={amount}
                         onChange={({ target }) => setAmount(target.value)}
-                        onClick={exchangeRate}
                     />
                 </label>
             </p>
