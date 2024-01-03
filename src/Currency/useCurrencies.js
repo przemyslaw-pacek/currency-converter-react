723e7756455c6537cb480a10406f18b2313d9ee4
Przemysław Pacek <ppacek@op.pl>
2024-01-03 16:07:33 +0100

    removed console.log

--- src/Currency/useCurrencies.js
+++ src/Currency/useCurrencies.js
@@ -1,33 +1,31 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const URL = "currencies.json"
-//"https://api.currencyapi.com/v3/latest?apikey=cur_live_6fdXdtntZGuUZUAm7tbYEWr6J2JjNvedZhbzYMIh";
+const URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_6fdXdtntZGuUZUAm7tbYEWr6J2JjNvedZhbzYMIh";
 
 export const useCurrencies = () => {
     const [data, setData] = useState({
         status: "loading",
     });
 
-        const getData = async () => {
-            try {
-                const response = await axios.get(URL);
+    const getData = async () => {
+        try {
+            const response = await axios.get(URL);
 
-                console.log(data.data);
-                setData({
-                    status: "content",
-                    data: response.data,
-                });
-            } catch {
-                setData({
-                    status: "error",
-                });
-            }
-        };
+            setData({
+                status: "content",
+                data: response.data,
+            });
+        } catch {
+            setData({
+                status: "error",
+            });
+        }
+    };
 
-        useEffect(() => {
-            setTimeout(getData, 1000);
+    useEffect(() => {
+        setTimeout(getData, 1000);
     }, []);
 
     return data;
-}
+};
