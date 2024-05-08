47cb0e33d456d3dc9194d41761a40df6781aa0e2
Przemysław Pacek <ppacek@op.pl>
2024-05-08 16:17:44 +0200

    added $ to props to avoid console errors and changed 'p' to 'span'

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -3,13 +3,13 @@ import { Alert } from "./styled.js";
 const Loading = ({ data }) => (
         <>
             {data.status === "loading" && (
-                <Alert loading>
+                <Alert $loading>
                     <p>Ładowanie...</p>
                     <p>Pobieranie danych z serwera.</p>
                 </Alert>
             )}
             {data.status === "error" && (
-                <Alert error>
+                <Alert $error>
                     <p>Coś poszło nie tak.</p>
                     <p>Brak połączenia z serwerem.</p>
                     <p>Spróbuj ponownie!</p>
--- src/Loading/styled.js
+++ src/Loading/styled.js
@@ -1,15 +1,15 @@
 import styled, { css } from "styled-components";
 
-export const Alert = styled.p`
+export const Alert = styled.span`
     text-align: center;
     font-size: 28px;
     font-weight: bold;
 
-    ${({ loading }) => loading && css`    
+    ${({ $loading }) => $loading && css`    
         color: ${({ theme }) => theme.color.yellow};
     `}
 
-    ${({ error }) => error && css`    
+    ${({ $error }) => $error && css`    
         color: ${({ theme }) => theme.color.tainoi};
     `}
 `;
