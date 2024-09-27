2b7628ea6a2cc31fa717065af4051b2edfc7df4d
Przemysław Pacek <ppacek@op.pl>
2024-09-27 09:28:22 +0200

    added empty dependency table

--- src/Clock/useCurrentDate.js
+++ src/Clock/useCurrentDate.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
 
 export const useCurrentDate = () => {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
 
-        return () => {
-            clearInterval(intervalId);
-        };
-    });
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return date;
+  return date;
 };
