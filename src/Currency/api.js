2eacb31eb0981139e37072af510299a00903ed6a
Przemysław Pacek <ppacek@op.pl>
2024-10-03 20:49:57 +0200

    function simplified

--- src/Currency/api.js
+++ src/Currency/api.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const URL =
   "https://api.currencyapi.com/v3/latest?apikey=cur_live_6fdXdtntZGuUZUAm7tbYEWr6J2JjNvedZhbzYMIh";
 
-export const getApi = async () => {
-  const response = await axios.get(URL);
-  const data = response.data;
-
-  return data;
+export const getApi = () => {
+  return axios.get(URL).then((response) => response.data);
 };
