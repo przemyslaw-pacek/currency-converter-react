d66a6aabd1e1220366b2062d6ff4cfcdeac7eb97
Przemysław Pacek <ppacek@op.pl>
2024-11-25 21:21:05 +0100

    fixed NaN bug for value attribute

--- src/Form/Amount/index.tsx
+++ src/Form/Amount/index.tsx
@@ -9,24 +9,44 @@ interface AmountProps {
   setAmount: (amount: number | null) => void;
 }
 
-const Amount = ({ ratesData, title, amount, setAmount }: AmountProps) => (
-  <>
-    {ratesData.status === "success" && (
-      <p>
-        <label>
-          <Title>{title}</Title>
-          <Input
-            required
-            type="number"
-            step="0.01"
-            min="0.01"
-            value={amount !== null ? amount : ""}
-            onChange={({ target }) => setAmount(parseFloat(target.value))}
-          />
-        </label>
-      </p>
-    )}
-  </>
-);
+const Amount = ({ ratesData, title, amount, setAmount }: AmountProps) => {
+  const handleInputChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    const value = target.value;
+
+    if (value === "") {
+      setAmount(null);
+      return;
+    }
+
+    const parsedValue = parseFloat(value);
+
+    if (!isNaN(parsedValue)) {
+      setAmount(parsedValue);
+      return;
+    }
+  };
+
+  return (
+    <>
+      {ratesData.status === "success" && (
+        <p>
+          <label>
+            <Title>{title}</Title>
+            <Input
+              required
+              type="number"
+              step="0.01"
+              min="0.01"
+              value={amount !== null ? amount : ""}
+              onChange={handleInputChange}
+            />
+          </label>
+        </p>
+      )}
+    </>
+  );
+};
 
 export default Amount;
